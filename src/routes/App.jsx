5b29f1bc335ec850b71329ad9142cad61fb2404b
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-10-25 21:18:43 -0500

    perfecto problema de versiones

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes , Route } from "react-router-dom";
+import { BrowserRouter , Switch,  Routes , Route } from "react-router-dom";
 import Layout from "../containers/Layout";
 import Login from "../containers/Login";
 import RecoveryPassword from "../containers/RecoveryPassword";
