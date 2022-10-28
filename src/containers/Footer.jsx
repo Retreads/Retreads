888c232239b2e1a31e1ec842c8d52176205c9518
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-10-27 22:51:01 -0500

    listo el footer

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import '../styles/global.css'
 
 const Footer = () => {
     return (
         <div className="footer">
             <div className="footer-container">
-                <p>&#169; Mixtos.com.co</p>
-                <p>Todos los derechos reservados / Restaurante de Comidas Mixtas</p>
-                <p> Calle 18A No.19-04 /Barrio Mancera Acacias - Meta</p>
+                
+                <p>
+                &#169; Mixtos.com.co           
+                <br />
+                Todos los derechos reservados / Restaurante de Comidas Mixtas
+                <br />
+                Calle 18A No.19-04 /Barrio Mancera Acacias - Meta
+                </p>
             </div>
         </div>
     );
