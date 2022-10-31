54a09cb7d4a1bdd66295271a58c34a34136e107b
NextorJP <111332593+NextorJP@users.noreply.github.com>
2022-10-31 17:47:31 -0500

    Ingreso de imagenes y descripcion de productos

---
+++ src/components/Item1CalentaoValluno.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import '../styles/global.css';
+import ButtonComprar from './ButtonComprar';
+import logo from "../asset/logoicono.png";
+
+export const Item1CalentaoValluno = () => {
+  return (
+    <>
+    <div className="card" /* style="width: 18rem" */>
+        <img className="card-img-top" src={logo}  alt="Card image cap" />
+        <div className="card-body">
+            <h5 className="card-title">Calentao Valluno</h5>
+            <p className="card-text">Esta es la descripcion del producto menos su valor o precio</p>
+            <h6 className='precioProducto'>Precio: &nbsp; &nbsp;	&#36; 8000</h6>
+            <ButtonComprar/>
+            
+        </div>
+    </div>
+    </>
+  )
+}
