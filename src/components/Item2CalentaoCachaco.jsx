d2f80445144ff0518cb70fac7e6305f88945b68f
NextorJP <111332593+NextorJP@users.noreply.github.com>
2022-11-01 15:51:19 -0500

    ajuste codigo en el menu

--- src/components/Item2CalentaoCachaco.jsx
+++ src/components/Item2CalentaoCachaco.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react'
 
 import "../styles/global.css";
 import ButtonComprar from "./ButtonComprar";
-import logo from "../asset/logoicono.png";
+import foto2 from '../asset/2CalentaoCachaco.jpg';
 
 export const Item2CalentaoCachaco = () => {
   return (
     <>
       <div className="card" /* style="width: 18rem" */>
-        <img className="card-img-top" src={logo} alt="Card image cap" />
+        <img className="rounded-square card-img-top" width="200" height="200" src={foto2} alt="..." />  
         <div className="card-body">
           <h5 className="card-title">CALENTAO CACHACO</h5>
           <p className="card-text">
@@ -19,7 +19,7 @@ export const Item2CalentaoCachaco = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Item1CalentaoValluno;
+export default Item2CalentaoCachaco;
