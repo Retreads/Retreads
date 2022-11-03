88274e1865a571887a6887edfd0ee74d611bdf6f
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-11-02 23:24:42 -0500

    cambios en routes ok

---
+++ src/components/BtnRecoger.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BtnRecoger = () => {
+    return (
+        <div className="cont-recoger-tienda">
+            <div className="inter-compras ">
+                <p>
+                <i className="fa-solid fa-cart-shopping"></i> &nbsp; &nbsp; Comprar 
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default BtnRecoger;
